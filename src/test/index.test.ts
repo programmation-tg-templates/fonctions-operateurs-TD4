dabd731f958d4b8a1f873517b77ee7cd0a2d9f8d
François Roland <francois.roland@heh.be>
2025-09-21 18:20:02 +0200

    Integrate Gianni's tests.

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,23 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {estMultiple, estImpairs} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('Vérifier si un nombre est un multiple d’un autre', () => {
+    test('15 est un multiple de 5', () => {
+        expect(estMultiple(15, 5)).toEqual(true);
+    });
+
+    test('14 n’est pas un multiple de 3', () => {
+        expect(estMultiple(14, 3)).toEqual(false);
+    });
+});
+
+describe('Vérifier si ces nombres sont impairs ', () => {
+    test('15 et 5 sont des nombres impairs.', () => {
+        expect(estImpairs(15, 5)).toEqual(true);
+    });
+
+    test('14 et 3 ne sont pas des nombres impairs.', () => {
+        expect(estImpairs(14, 3)).toEqual(false);
     });
 });
